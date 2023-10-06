cb14f7c4c126cb134cce2525683117b7298ee917
Tretiak Serhii <tretyak8936@gmail.com>
2023-10-07 00:19:01 +0300

    implemented a component for latest news

---
+++ src/components/LatestNewsItem/LatestNewsItem.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const LatestNewsItem: React.FC = React.memo(() => (
+  <div className="news-item">
+    <p className="news-item__title">How to Learn Coding for Beginners</p>
+    <p className="news-item__data">28 Feb 2021</p>
+  </div>
+));
