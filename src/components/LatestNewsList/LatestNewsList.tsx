e64514dc3ec172caf7ccf5b73d509b199cf9fe5b
Tretiak Serhii <tretyak8936@gmail.com>
2023-10-09 13:33:41 +0300

    changed some logic for components and design

--- src/components/LatestNewsList/LatestNewsList.tsx
+++ src/components/LatestNewsList/LatestNewsList.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ArticleType } from '../../types';
 import { LatestNewsItem } from '../LatestNewsItem';
 import './LatestNewsList.scss';
+import { Service } from '../../api/service';
 
-export const LatestNewsList = () => (
-  <div className="news-list">
-    <p className="news-list__title">Our Latest News</p>
+export const LatestNewsList: React.FC = () => {
+  const [lastNews, setLastNews] = useState<ArticleType[]>([]);
 
-    <LatestNewsItem />
-    <LatestNewsItem />
-    <LatestNewsItem />
-  </div>
-);
+  const fetchLatestNews = async () => {
+    try {
+      const data = await Service.getLatestNews();
+
+      setLastNews(data);
+    } catch (error) {
+      throw new Error('Downloading error');
+    }
+  };
+
+  useEffect(() => {
+    fetchLatestNews();
+  }, []);
+
+  return (
+    <div className="news-list">
+      <p className="news-list__title">Our Latest News</p>
+
+      {lastNews.map((article) => (
+        <LatestNewsItem key={article.id} article={article} />
+      ))}
+    </div>
+  );
+};
--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import Pagination from '@mui/material/Pagination';
-// import { Article } from '../../components/Article';
-// import { LatestNewsList } from '../../components/LatestNewsList';
+import { Article } from '../../components/Article';
+import { LatestNewsList } from '../../components/LatestNewsList';
 import './Blog.scss';
 import { ArticleType } from '../../types';
 import { Service } from '../../api/service';
@@ -10,6 +10,7 @@ import { InfoArticle } from '../../components/InfoArticle';
 export const Blog: React.FC = () => {
   const [articles, setArticle] = useState<ArticleType[]>([]);
   const [page, setPage] = useState(1);
+  const [count, setCount] = useState(0);
 
   const getArticles = async () => {
     try {
@@ -21,9 +22,21 @@ export const Blog: React.FC = () => {
     }
   };
 
+  const fetchCount = async () => {
+    const data = await Service.getCount();
+
+    const preparedCount = Math.ceil(data / 6);
+
+    setCount(preparedCount);
+  };
+
   useEffect(() => {
     getArticles();
   }, [page]);
+
+  useEffect(() => {
+    fetchCount();
+  }, []);
 
   const handlerPageChange = (e: ChangeEvent<unknown>, p: number) => {
     setPage(p);
@@ -32,10 +45,12 @@ export const Blog: React.FC = () => {
   return (
     <div className="blog">
       <div className="blog__container">
-        {/* <div className="blog__news-wrapper">
+        <div className="blog__news-wrapper">
           <Article />
           <LatestNewsList />
-        </div> */}
+        </div>
+
+        <Article />
 
         <div className="blog__pagination">
           {articles.map((article) => (
@@ -43,7 +58,7 @@ export const Blog: React.FC = () => {
           ))}
         </div>
 
-        <Pagination count={5} color="primary" onChange={handlerPageChange} />
+        <Pagination count={count} color="primary" onChange={handlerPageChange} />
       </div>
     </div>
   );
